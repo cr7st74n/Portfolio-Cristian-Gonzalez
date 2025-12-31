b5c4877e10011f486c85e4ccb209050d90c4439d
cr7st74n <cris-082011@hotmail.com>
2025-12-31 14:27:34 -0500

    modify the routes to new domain

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Projects from './pages/Projects';
-import Info from './pages/Info';
+import Projects from "./pages/Projects";
+import Info from "./pages/Info";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/Portfolio-Cristian-Gonzalez" element={<Home />}/>
-          <Route path="/projects" element={<Projects />}/>
-          <Route path="/info" element={<Info />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/info" element={<Info />} />
         </Routes>
         <Footer />
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
